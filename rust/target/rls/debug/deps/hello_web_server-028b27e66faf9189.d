8054354e7db59377eebc61924f0aedf21d9fdd94
Ben Ukhanov <ben1zuk321@gmail.com>
2021-02-26 16:32:24 +0200

    :truck: src: add rust folder

---
+++ rust/target/rls/debug/deps/hello_web_server-028b27e66faf9189.d
@@ -0,0 +1,5 @@
+/home/ben/GitHub/kinto-examples/rust/target/rls/debug/deps/hello_web_server-028b27e66faf9189.rmeta: src/main.rs
+
+/home/ben/GitHub/kinto-examples/rust/target/rls/debug/deps/hello_web_server-028b27e66faf9189.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/rls/debug/deps/hello_web_server-60e99de6dd6cb702.d
@@ -0,0 +1,5 @@
+/home/ben/GitHub/kinto-examples/rust/target/rls/debug/deps/hello_web_server-60e99de6dd6cb702.rmeta: src/main.rs
+
+/home/ben/GitHub/kinto-examples/rust/target/rls/debug/deps/hello_web_server-60e99de6dd6cb702.d: src/main.rs
+
+src/main.rs:
